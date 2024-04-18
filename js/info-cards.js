332f92eb0e7852c8f07c36d6a203e2fe8ea62cee
jacksonhuish88 <jacksonhuish88@gmail.com>
2024-04-18 01:15:17 -0600

    icon adjustments

--- js/info-cards.js
+++ js/info-cards.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const infoItems = [
         {
-            icon: "fas fa-clock fa-3x",
+            icon: "fas fa-trophy fa-3x",
             title: "24/7 Champions",
             text: "Our service is like a superhero – doesn't take a day off. Whether it’s a holiday or your in-laws’ surprise visit, we're at your service at any hour – because sleep is overrated, right?"
         },
         {
-            icon: "fas fa-heartbeat fa-3x",
+            icon: "fa-solid fa-hand-holding-heart fa-3x",
             title: "Pampering Perfectionists",
             text: "We handle every home and vehicle with the kind of tender care usually reserved for a baby kangaroo in a pouch. Our team's attention to detail might just make your inanimate objects feel loved!"
         },
         {
-            icon: "fas fa-home fa-3x",
+            icon: "fas fa-location-crosshairs fa-3x",
             title: "Hometown Homies",
             text: "Straight outta Provo, we bring neighborly love to every job. We're the local lore legends, turning your chores into cheers. Keeping it small, keeping it real – that’s the Utah deal!"
         }
